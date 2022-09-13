0c1777f44e96d7f05f89d3e1baae8e2597bf0adc
melgert <mdelgert@yahoo.com>
2022-09-13 19:39:53 -0400

    All null ConfigurationModel

--- AzureProject.Shared/Model/ConfigurationModel.cs
+++ AzureProject.Shared/Model/ConfigurationModel.cs
@@ -2,6 +2,6 @@
 
 public class ConfigurationModel
 {
-    public string AzureKeyVault { get; set; }
-    public string OpenWeatherKey { get; set; }
+    public string? AzureKeyVault { get; set; }
+    public string? OpenWeatherKey { get; set; }
 }

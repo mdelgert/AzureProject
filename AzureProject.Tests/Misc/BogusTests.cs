18f5547bd127ee6b18eaba28b07a3a634ad94ff9
melgert <mdelgert@yahoo.com>
2022-09-13 18:14:54 -0400

    Add BogusTests

---
+++ AzureProject.Tests/Misc/BogusTests.cs
@@ -0,0 +1,34 @@
+﻿namespace AzureProject.Tests.Misc;
+
+public class BogusTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public BogusTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void AddressTest()
+    {
+        var faker = new Faker();
+        var t = faker.Address;
+        var address = $"{t.BuildingNumber()} {t.StreetName()} {t.City()}, {t.State()} {t.ZipCode()}";
+        _testOutputHelper.WriteLine(address);
+        Assert.NotNull(address);
+    }
+
+    [Fact]
+    public void PhoneNumberTest()
+    {
+        var faker = new Faker();
+        var p = faker.Phone;
+        var phoneNumber = $"{p.PhoneNumber()}";
+        _testOutputHelper.WriteLine(phoneNumber);
+        Assert.NotNull(phoneNumber);
+    }
+}
+
+//https://www.nuget.org/packages/Bogus
+//https://github.com/bchavez/Bogus

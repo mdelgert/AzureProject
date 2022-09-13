a4f34f70925a372594f49978a5685032c5f965e2
melgert <mdelgert@yahoo.com>
2022-09-13 00:38:43 -0400

    Add StringExtHelper

---
+++ AzureProject.Shared/Helpers/StringExtHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureProject.Shared.Helpers;
+
+public static class StringExtHelper
+{
+    public static string? Truncate(this string? value, int maxLength, string truncationSuffix = "…")
+    {
+        return value?.Length > maxLength ? value[..maxLength] + truncationSuffix : value;
+    }
+}
+
+//https://stackoverflow.com/questions/2776673/how-do-i-truncate-a-net-string

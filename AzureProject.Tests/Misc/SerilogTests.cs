562a84564d07f429d38c31e0816b0de0108e3ace
melgert <mdelgert@yahoo.com>
2022-09-13 18:15:27 -0400

    Add SerilogTests

---
+++ AzureProject.Tests/Misc/SerilogTests.cs
@@ -0,0 +1,19 @@
+﻿namespace AzureProject.Tests.Misc;
+
+public class SerilogTests
+{
+    private readonly ILogger<SerilogTests> _log;
+
+    public SerilogTests(ILogger<SerilogTests> log)
+    {
+        _log = log;
+    }
+
+    [Fact]
+    public void LogTest()
+    {
+        _log.LogInformation("Hello logger!");
+    }
+}
+
+//https://stackify.com/net-core-loggerfactory-use-correctly/

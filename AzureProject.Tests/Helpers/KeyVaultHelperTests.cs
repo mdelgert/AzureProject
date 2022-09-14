03d2f135a0b075490cc1dd1f1c102444980a15a4
melgert <mdelgert@yahoo.com>
2022-09-14 13:24:38 -0400

    Update key vault unit test

--- AzureProject.Tests/Helpers/KeyVaultHelperTests.cs
+++ AzureProject.Tests/Helpers/KeyVaultHelperTests.cs
@@ -2,9 +2,18 @@
 
 public class KeyVaultHelperTests
 {
+    // [Theory]
+    // [InlineData("Test3", "12345")]
+    // public async Task SetSecret(string secretName, string secretValue)
+    // {
+    //     await KeyVaultHelper.SetSecret(secretName, secretValue);
+    //     var keyVaultValue = await KeyVaultHelper.GetSecret(secretName);
+    //     Assert.Equal(keyVaultValue, secretValue);
+    // }
+    
     [Theory]
-    [InlineData("Test", "12345")]
-    public async Task KeyVaultHelperTest(string secretName, string secretValue)
+    [InlineData("Test3", "12345")]
+    public async Task GetSecretTest(string secretName, string secretValue)
     {
         var keyVaultValue = await KeyVaultHelper.GetSecret(secretName);
         Assert.Equal(keyVaultValue, secretValue);

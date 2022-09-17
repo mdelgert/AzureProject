95607b534ae1b01ee092a4cf857f36ed2ba381c5
melgert <mdelgert@yahoo.com>
2022-09-17 02:41:04 -0400

    Update bogus unit tests.

--- AzureProject.Tests/Misc/BogusTests.cs
+++ AzureProject.Tests/Misc/BogusTests.cs
@@ -1,10 +1,13 @@
 ﻿//Nuget: https://www.nuget.org/packages/Bogus
+
+using System.Globalization;
 using Bogus;
 
 namespace AzureProject.Tests.Misc;
 
 public class BogusTests
 {
+    private readonly Faker _faker = new Faker();
     private readonly ITestOutputHelper _testOutputHelper;
 
     public BogusTests(ITestOutputHelper testOutputHelper)
@@ -13,10 +16,46 @@ public class BogusTests
     }
 
     [Fact]
+    public void RandomArrayStringTest()
+    {
+        string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        _testOutputHelper.WriteLine(_faker.Random.ArrayElement(cars));
+    }
+    
+    [Fact]
+    public void RandomAlphaNumericTest()
+    {
+        _testOutputHelper.WriteLine(_faker.Random.AlphaNumeric(10));
+    }
+    
+    [Fact]
+    public void LoremTest()
+    {
+        _testOutputHelper.WriteLine(_faker.Lorem.Paragraph());
+    }
+
+    [Fact]
+    public void CatchPhraseTest()
+    {
+        _testOutputHelper.WriteLine(_faker.Company.CatchPhrase());
+    }
+
+    [Fact]
+    public void DateTest()
+    {
+        _testOutputHelper.WriteLine(_faker.Date.Past().ToString(CultureInfo.InvariantCulture));
+    }
+
+    [Fact]
+    public void PasswordTest()
+    {
+        _testOutputHelper.WriteLine(_faker.Internet.Password(length:50));
+    }
+    
+    [Fact]
     public void AddressTest()
     {
-        var faker = new Faker();
-        var t = faker.Address;
+        var t = _faker.Address;
         var address = $"{t.BuildingNumber()} {t.StreetName()} {t.City()}, {t.State()} {t.ZipCode()}";
         _testOutputHelper.WriteLine(address);
         Assert.NotNull(address);
@@ -25,12 +64,12 @@ public class BogusTests
     [Fact]
     public void PhoneNumberTest()
     {
-        var faker = new Faker();
-        var p = faker.Phone;
+        var p = _faker.Phone;
         var phoneNumber = $"{p.PhoneNumber()}";
         _testOutputHelper.WriteLine(phoneNumber);
         Assert.NotNull(phoneNumber);
     }
+    
 }
 
 //https://www.nuget.org/packages/Bogus
